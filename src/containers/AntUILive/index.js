d1ae5b4da12dac948068d54f18cb8a956070b8cd
fullstack <david.elbel@fullstackwebdevelopment.com>
2019-01-29 12:44:48 -0800

    Refactor into functional components

--- src/containers/AntUILive/index.js
+++ src/containers/AntUILive/index.js
@@ -4,136 +4,142 @@ import "./index.css";
 import SubMenu from "antd/lib/menu/SubMenu";
 const { Header, Footer, Sider, Content } = Layout;
 
+const MyContent = ({breadcrumbItems}) => {
+  // const breadcrumbItems = ["Home", "List", "App"];
+  return (
+    <>
+      {/* Breadcrumb */}
+      <Breadcrumb style={{ margin: "16px 0" }}>
+        {breadcrumbItems.map((item, index) => (
+          <Breadcrumb.Item>{item}</Breadcrumb.Item>
+        ))}
+      </Breadcrumb>
 
-const MyHeader = () => (
-  <Header className="header">
-  <Row type="flex">
- <Col xs={24} sm={6}> <div className="logo" />
- Live Theme
- </Col>
- 
- <Col xs={0} sm={18}>
- <Menu
-    theme="dark"
-    mode="horizontal"
-    defaultSelectedKeys={["2"]}
-    style={{ lineHeight: "64px" }}
-  >
-    <Menu.Item key="1">nav 1</Menu.Item>
-    <Menu.Item key="2">nav 2</Menu.Item>
-    <Menu.Item key="3">nav 3</Menu.Item>
-  </Menu>
-  </Col>
+      {/* Main content */}
+      <Content
+        style={{
+          background: "#fff",
+          padding: 24,
+          margin: 0,
+          minHeight: 280
+        }}
+      >
+        <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
+          Content
+        </div>
+      </Content>
+    </>
+  );
+};
 
-  </Row>
-</Header>
-)
+const MyHeader = ({headers}) => {
+  //const {headers} = props;// ["Nav", "Nav 2", "Nav 3"];
+  return (
+    <Header className="header">
+      <Row type="flex">
+        <Col xs={24} sm={6}>
+          {" "}
+          <div className="logo" />
+          Live Theme
+        </Col>
+
+        <Col xs={0} sm={18}>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={["2"]}
+            style={{ lineHeight: "64px" }}
+          >
+            {headers.map((item, index) => (
+              <Menu.Item key={index}>{item}</Menu.Item>
+            ))}
+          </Menu>
+        </Col>
+      </Row>
+    </Header>
+  );
+};
+const MySideBar = ({sideMenu}) => {
+  return (
+    <Sider width={200}>
+      <Menu
+        mode="inline"
+        defaultSelectedKeys={["1"]}
+        defaultOpenKeys={["sub1"]}
+        style={{ height: "100%", borderRight: 0 }}
+      >
+        {sideMenu.map((menu, index) => (
+          <SubMenu
+            key={index}
+            title={
+              <span>
+                <Icon type={menu.icon} />
+                {menu.name}
+              </span>
+            }
+          >
+            {menu.sub.map((item, index) => (
+              <Menu.Item key={index}>{item}</Menu.Item>
+            ))}
+          </SubMenu>
+        ))}
+      </Menu>
+    </Sider>
+  );
+};
 
 //https://antd-live-theme.firebaseapp.com/
+
+const myHeaders = ["Nav", "Nav 2", "Nav 3"];
+const breadcrumbItems = ["Home", "List", "App"];
+const sideMenu = [
+  {
+    name: "First Submenu",
+    icon: "user",
+    sub: ["option1", "option2", "option3", "option4"]
+  },
+  {
+    name: "Second Submenu",
+    icon: "laptop",
+    sub: ["option5", "option6", "option7", "option8"]
+  },
+  {
+    name: "Third Submenu",
+    icon: "notification",
+    sub: ["option9", "option11", "option12", "option13"]
+  }
+];
 
 const index = () => {
   return (
     <>
-    <Row>
-      <Layout style={{ height: "100vh" }}>
-        {/* <Layout> */}
-        <MyHeader />
-          {/* <Header className="header">
-            <Row type="flex">
-           <Col xs={24} sm={6}> <div className="logo" />
-           Live Theme
-           </Col>
-           
-           <Col xs={0} sm={18}>
-           <Menu
-              theme="dark"
-              mode="horizontal"
-              defaultSelectedKeys={["2"]}
-              style={{ lineHeight: "64px" }}
-            >
-              <Menu.Item key="1">nav 1</Menu.Item>
-              <Menu.Item key="2">nav 2</Menu.Item>
-              <Menu.Item key="3">nav 3</Menu.Item>
-            </Menu>
-            </Col>
+      <Row>
+        <Layout style={{ height: "100vh" }}>
 
-            </Row>
-          </Header> */}
-        {/* </Layout> */}
-        <Layout>
-          <Sider width={200}>
-            <Menu
-              mode="inline"
-              defaultSelectedKeys={["1"]}
-              defaultOpenKeys={["sub1"]}
-              style={{ height: "100%", borderRight: 0 }}
-            >
-              <SubMenu
-                key="sub1"
-                title={
-                  <span>
-                    <Icon type="user" />
-                    First submenu
-                  </span>
-                }
-              >
-                <Menu.Item key="1">option1</Menu.Item>
-                <Menu.Item key="2">option2</Menu.Item>
-                <Menu.Item key="3">option3</Menu.Item>
-                <Menu.Item key="4">option4</Menu.Item>
-              </SubMenu>
-              <SubMenu 
-                key="sub2" 
-                title={<span > <Icon type="laptop" />Second submenu</span>}>
-                <Menu.Item key="5">Option5</Menu.Item>
-                <Menu.Item key="6">Option5</Menu.Item>
-                <Menu.Item key="7">Option5</Menu.Item>
-                <Menu.Item key="8">Option5</Menu.Item>
-              </SubMenu>
-              <SubMenu
-                    key="sub3"
-                    title={
-                      <span>
-                        <Icon type="notification" />Third submenu
-                      </span>
-                    }
-                  >
-                    <Menu.Item key="9">option9</Menu.Item>
-                    <Menu.Item key="10">option10</Menu.Item>
-                    <Menu.Item key="11">option11</Menu.Item>
-                    <Menu.Item key="12">option12</Menu.Item>
-                  </SubMenu>
-            </Menu>
-          </Sider>
-       
+          {/* Header */}
+          <MyHeader headers={myHeaders}/>
 
-        <Layout  style={{ padding: "0 24px 24px", minHeight: "100vh" }}>
-                    <Breadcrumb style={{ margin: "16px 0" }}>
-                    <Breadcrumb.Item>Home</Breadcrumb.Item>
-                    <Breadcrumb.Item>List</Breadcrumb.Item>
-                    <Breadcrumb.Item>App</Breadcrumb.Item>
-                  </Breadcrumb>
-          <Content
-          style={{
-            background: "#fff",
-            padding: 24,
-            margin: 0,
-            minHeight: 280
-          }}
-        >
+          <Layout>
 
-            <div style={{ background: "#fff", padding: 24, minHeight: 280 }}>
-              Content
-            </div>
-          </Content>
-        </Layout>
-        </Layout>
-        <Layout>
+            {/* Sidebar */}
+            <MySideBar sideMenu={sideMenu}/>
+
+            <Layout style={{ padding: "0 24px 24px", height: "100vh" }}>
+
+              {/* Content */}
+              <MyContent breadcrumbItems={breadcrumbItems}/>
+
+            </Layout>
+          </Layout>
           <Footer style={{ textAlign: "center" }}>
-            Ant Design ©2018 Created by Ant UED
-          </Footer>
+              FSWD ©2019 Created by D
+            </Footer>
         </Layout>
-      </Layout>
+        {/* <Layout>
+            <Footer style={{ textAlign: "center" }}>
+              FSWD ©2019 Created by D
+            </Footer>
+          </Layout> */}
       </Row>
     </>
   );
